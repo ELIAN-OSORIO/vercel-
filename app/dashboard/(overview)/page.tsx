8e13ec38bb4de216c2edf38887667b8e9283de5e
ELIAN-OSORIO <al19760601@ite.edu.mx>
2024-02-12 17:23:03 -0800

    arreglo-error

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,8 +1,8 @@
-import CardWrapper from '../ui/dashboard/cards';
+import CardWrapper from '../../ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '../lib/data';
+import { fetchCardData } from '../../lib/data';
 import { Suspense } from 'react';
 import {
   RevenueChartSkeleton,
